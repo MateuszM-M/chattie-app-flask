23843d8cac66c6e65f8d3eb8aa3d566a7779c18b
MateuszM-M <mateuszmikulski2@gmail.com>
2022-01-22 17:36:20 +0100

    Start adding log in and register

--- chattie/models.py
+++ chattie/models.py
@@ -1,15 +1,27 @@
-from chattie import db
+from datetime import datetime
+from chattie import db, login_manager
 from flask_login import UserMixin
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
 
 
-class Room(db.Model):
-    pass
+    # class Room(db.Model):
+    #     pass
 
 
-class Message(db.Model):
-    pass
+    # class Message(db.Model):
+    #     pass
